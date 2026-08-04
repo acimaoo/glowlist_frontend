c1c7059b8e318daf947228ba0c1421fbfdab9dcb
acimaoo <charismaeka0@gmail.com>
2026-08-04 15:05:32 +0700

    menambahkan jarak di header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <Link to="/" className="navbar-brand">MaoGlow</Link>
-            <button className="btn btn-danger">Logout</button>
+            <Link to="/" className="navbar-brand text-white ps-4">MaoGlow</Link>
+            <button className="btn btn-danger ms-auto me-4">Logout</button>
         </nav>
     )
 }
