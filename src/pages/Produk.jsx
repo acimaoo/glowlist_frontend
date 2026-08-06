0ba045aef89d60761cad709bb4e10cb1a4690a27
acimaoo <charismaeka0@gmail.com>
2026-08-06 20:59:45 +0700

    penambahan handle delete di halaman produk

--- src/pages/Produk.jsx
+++ src/pages/Produk.jsx
@@ -21,6 +21,29 @@ export default function Produk() {
         getProduk();
     }, []);
 
+    const handleDelete = async (id) => {
+        if (window.confirm("Yakin ingin menghapus produk ini?")) {
+            try {
+                const res = await fetch(`http://localhost:5000/produk/${id}`, {
+                    method: "DELETE",
+                })
+                if (res.ok) {
+                    alert("Produk berhasil dihapus!");
+                    getProduk(); //mengambil ulang data terbaru
+                } else {
+                    alert("Gagal menghapus produk");
+                }
+            } catch (err) {
+                console.error("Error saat delete:", err);
+                alert("Terjadi kesalahan saat menghapus data");
+            }
+        }
+    }
+
+    const handleEdit = (id) => {
+        Navigate(`/produk/edit/${id}`)
+    }
+
     if (loading) {
         return <div className="container mt-4"> Sedang Memuat Data... </div>
     }
@@ -29,7 +52,7 @@ export default function Produk() {
         <div className="container mt-4">
             <div className="d-flex justify-content-between align-items-center mb-3">
                 <h2>Daftar Produk</h2>
-                <Link to="/produk/tambah" className="btn btn-primary">Tambah Produk</Link>
+                <Link to="/produk/tambah" className="btn btn-primary">+ Produk</Link>
             </div>
             <table className="table table-bordered table-striped">
                 <thead className="table-primary">
@@ -38,6 +61,7 @@ export default function Produk() {
                         <th>Produk</th>
                         <th>Deskripsi</th>
                         <th>Harga</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -48,6 +72,18 @@ export default function Produk() {
                                 <td>{item.judul}</td>
                                 <td>{item.deskripsi}</td>
                                 <td>Rp {item.harga.toLocaleString()}</td>
+                                <td>
+                                    <div className="d-flex gap-1">
+                                    <button
+                                        className="btn btn-warning btn-sm me-2"
+                                        onClick={() => handleEdit(item.id_produk)}
+                                    >Edit</button>
+                                    <button
+                                        className="btn btn-danger btn-sm"
+                                        onClick={() => handleDelete(item.id_produk)}
+                                    >Delete</button>
+                                    </div>
+                                </td>
                             </tr>
                         ))
                     ) : (
